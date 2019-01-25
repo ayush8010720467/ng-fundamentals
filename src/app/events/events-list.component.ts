a9cb9c4c76db9a3d0f7321cfb5c11c5d46215874
ayush8010720467 <ayushkatiyar096@gmail.com>
2019-01-25 15:25:04 +0530

    implemented class = well in bootstrap

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -5,9 +5,29 @@ import { Component } from '@angular/core'
     template: `
     <div>
         <h1>Upcomming Angular Events </h1>
+        <hr/>
+        <div class = "well hoverwell thumbnail" >
+        <h2>{{event.name}}</h2>
+        <div>{{event.date}}</div>
+        <div>{{event.time}}</div>
+        <div>Price: \${{event.price}}</div>
+        </div>
     </div>
     `
 })
 export class  EventListComponent{
+    event = {
+        id: 1,
+        name: 'Angular Connect',
+        date: '25/01/18',
+        time: '03:09 am',
+        price: 599.9,
+        imageUrl:'../../assets/images/angularconnect-shield.png',
+        location:{
+            address: '975/5',
+            city: 'Kanpur',
+            country:'India'
+        }
+    }
 
 }
