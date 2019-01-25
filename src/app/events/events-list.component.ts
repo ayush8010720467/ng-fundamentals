d91b8663b0e0aea3de5127f979c27bb85458d504
ayush8010720467 <ayushkatiyar096@gmail.com>
2019-01-25 15:49:46 +0530

    Adding the template portion code into events-list.component.html file and by passing the url we are going to obtain the similar results

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,18 +2,7 @@ import { Component } from '@angular/core'
 
 @Component({
     selector : 'events-list',
-    template: `
-    <div>
-        <h1>Upcomming Angular Events </h1>
-        <hr/>
-        <div class = "well hoverwell thumbnail" >
-        <h2>{{event.name}}</h2>
-        <div>{{event.date}}</div>
-        <div>{{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
-        </div>
-    </div>
-    `
+    templateUrl: "./events-list.component.html"
 })
 export class  EventListComponent{
     event = {
