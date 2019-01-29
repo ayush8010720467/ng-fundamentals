1f10971f5f7ded4f52cf318a99e202fbbe783ddd
ayush8010720467 <ayushkatiyar096@gmail.com>
2019-01-29 12:04:54 +0530

    adding the css to the project

--- src/app/events/event-thumbnail.comopnent.ts
+++ src/app/events/event-thumbnail.comopnent.ts
@@ -8,15 +8,25 @@ import { Component, Input, EventEmitter } from '@angular/core';
     <div>{{event.date}}</div>
     <div>{{event.time}}</div>
     <div>Price: \${{event.price}}</div>
-   
+    <div>
+      <span>Location: {{event.location.address}}</span>
+      <span class = "pad-left">{{event.location.city}} , {{event.location.country}}</span>
+    </div> 
     </div>
     
+    `,
+    styles:[`
+      .pad-left { margin-left:10px; }
+      .well div {color: Red;
+                }
+      
     `
+  ]
+
+    
 })
 export class EventThumbnailComponent{
   @Input() event: any;
   someProperty: any = "some value" ;
-  logFoo(){
-      console.log("Foo");
-  }
+  
 }
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -6,14 +6,19 @@ import { Component  } from '@angular/core'
     <div>
     <h1>Upcomming Angular Events </h1>
     <hr/>
-    <event-thumbnail #thumbnail [event] = "event1"></event-thumbnail>
-    <h3>
-    {{thumbnail.someProperty}}
-    </h3>
-    <button class = "btn btn-primary" (click) = "thumbnail.logFoo()">
-    Log me some foo</button>
+    <div class = "well">
+    <div>Hello world</div>
+    </div>
+    <event-thumbnail  [event] = "event1"></event-thumbnail>
+    
 </div>
-    `
+    `,
+    styles: [
+        `
+        .well div {color: 	#FFFF00 ;
+        }
+        `
+    ]
 })
 export class  EventListComponent{
     event1 = {
